ccc486829fab63b1498376f73a696b7aa317a33a
cb-preethy.pk <cb-preethy.pk@CBIT01612.local>
2021-04-23 22:13:34 +0530

    remove unused code

--- python_whatsapp/bot.py
+++ python_whatsapp/bot.py
@@ -10,32 +10,12 @@ CB_APP_URL = os.environ['CB_APP_URL']
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    cb_url = CB_APP_URL#"http://cb-rajat.in.ngrok.io/message"
+    cb_url = CB_APP_URL
     cb_params = {'from': request.values.get('WaId'), 'message': request.values.get('Body')} 
     print("Posting request to: {} with params: {}".format(cb_url, cb_params))
     r = requests.post(cb_url, json=cb_params)
     print(r.status_code)
     return str({'status': 'ok'})
-#
-#    incoming_msg = request.values.get('Body', '').lower()
-#    resp = MessagingResponse()
-#    msg = resp.message()
-#    responded = False
-#    if 'quote' in incoming_msg:
-#        # return a quote
-##        r = requests.get('https://api.quotable.io/random')
-##        if r.status_code == 200:
-##            data = r.json()
-#        quote = "My quote for you Tadaa..."
-#        msg.body(quote)
-#        responded = True
-#    if 'cat' in incoming_msg:
-#        # return a cat pic
-#        msg.media('https://cataas.com/cat')
-#        responded = True
-#    if not responded:
-#        msg.body('I only know about famous quotes and cats, sorry!')
-#    return str(resp)
 
 @app.route('/send', methods=['POST'])
 def send():
@@ -46,7 +26,6 @@ def send():
     to_whatsapp_number = 'whatsapp:+{}'.format(request.form.get("to_whatsapp_number"))
     whatsapp_msg = request.form.get("txt_msg")
     message = client.messages.create(body=whatsapp_msg,
-#                           media_url='https://cataas.com/cat',
                            from_=from_whatsapp_number,
                            to=to_whatsapp_number)
 
